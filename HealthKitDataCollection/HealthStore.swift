95d644cd4c2c6c608d7a3812db5bccad949619f8
RyosukeKamimura <come1234560@gmail.com>
2020-10-20 14:39:24 +0900

    Get Walking Data🎊

--- HealthKitDataCollection/HealthStore.swift
+++ HealthKitDataCollection/HealthStore.swift
@@ -9,6 +9,12 @@ import Foundation
 import HealthKit
 
 
+extension Date {
+    static func mondayAt12AM() -> Date {
+        return Calendar(identifier: .iso8601).date(from: Calendar(identifier: .iso8601).dateComponents([.yearForWeekOfYear, .weekOfYear], from: Date()))!
+    }
+}
+
 class HealthStore{
     
     var healthStore: HKHealthStore?
@@ -29,6 +35,41 @@ class HealthStore{
             print("HealthKit Available")
         }else{
             print("Unavailable")
+        }
+    }
+    
+    // Execute Statistics Queries
+    func calculateSteps(completion: @escaping (HKStatisticsCollection?) -> Void) {
+        let stepType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
+        let startDate = Calendar.current.date(byAdding: .day, value: 7, to: Date())
+        
+        let anchorDay = Date.mondayAt12AM()
+        
+        let daily = DateComponents(day: 1)
+        
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: .strictStartDate)
+        
+        query = HKStatisticsCollectionQuery(quantityType: stepType, quantitySamplePredicate: predicate, options: .cumulativeSum, anchorDate: anchorDay, intervalComponents: daily)
+        
+        query!.initialResultsHandler = { query, statisticsCollection, error in
+            completion(statisticsCollection)
+            
+        }
+        
+        if let healthStore = healthStore, let query = self.query {
+            healthStore.execute(query)
+        }
+        
+    }
+    
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        let stepType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
+        guard let healthStore = self.healthStore else {
+            return completion(false)
+        }
+        healthStore.requestAuthorization(toShare: [], read: [stepType]){(success, error) in
+            completion(success)
         }
     }
 }
